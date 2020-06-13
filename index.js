086c40e05fdfe878ce46265e9e6cd58780d9cbc0
Oscar Martinez de la O <icehman@hotmail.com>
2020-06-13 04:23:09 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+/// una iteracion es una regla de codigo que se repite hasta que se alcanza una condicion
+
+/*
+-->   Imprime los números del 1 al 50.
+-->   Si el número es divisible por 7, debes omitir el siguiente número.
+-->   Si el número es divisible entre 10 o 15, debes imprimir “Donkey!”.
+-->   Si el número no es divisible por 2 y el número anterior es divisible por 10, debes imprimir “Monkey!”.
+*/
+//let ope = 0%10 
+//ºconsole.log(ope)
+
+for (let i = 1; i <= 50; i++){
+  if (i%7 === 0){
+    continue
+  } else if (i%2 !=0 && (i-1)%10===0){
+    console.log('“Monkey')
+  }else if ( i%10 === 0 ){
+    console.log('Donkey')
+  }else if (i%15 === 0) {
+    console.log('Donkey')
+  }else {
+  console.log(i)}
+}
